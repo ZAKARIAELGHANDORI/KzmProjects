aa2e3c7817aabbed2433eb1afd9858d0e26682dd
ZAKARIAELGHANDORI <z.elghandori@karizma.ma>
2022-12-22 10:27:20 +0100

    FROM PYCHARM : modifier le script2 pour crée un fchier avec un nom dynamique

--- script2.sh
+++ script2.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
+D=`date +%M`
+if [ -e fichier_$D.txt ]
+then
 
-touch /home/zakaria2001/Bureau/MyProjects/KzmProjects/fichier_`date +%M`.txt
-
+  touch /home/zakaria2001/Bureau/MyProjects/KzmProjects/fichier_$($D + 1).txt
+else
+  touch /home/zakaria2001/Bureau/MyProjects/KzmProjects/fichier_$D.txt
+fi
 
 ## on crée un fichier sous le nom fichier_minutes comme ca on va avoir un nom qui se change a chaque lancement de cron
 
 ###
 #pour lancer la création d'un fichier chaque -6heures on utilise la commande :
 #* */6 * * * path/to/script2.sh
-###
+####! /bin/bash
+
